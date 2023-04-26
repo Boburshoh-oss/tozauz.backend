71c1a6e7346dd44362ae496230d9adc0ffaaa9cc
Boburshoh-oss <boburbekbotirov868@gmail.com>
2023-04-26 11:10:39 +0500

    packet scan fixed

--- packet/views.py
+++ packet/views.py
@@ -11,7 +11,13 @@ from .models import Packet, Category
 from bank.models import Earning
 from ecopacket.models import Box, LifeCycle
 from rest_framework import viewsets, generics
-from .serializers import CategorySerializer, PacketSerializer, PacketSerializerCreate, PacketGarbageSerializer
+from .serializers import (
+    CategorySerializer,
+    PacketSerializer,
+    PacketSerializerCreate,
+    PacketGarbageSerializer,
+)
+from account.serializers import UserEarningSerializer
 from utils.pagination import MyPagination
 from django_filters import rest_framework as filters
 from rest_framework import filters as rf_filters
@@ -21,25 +27,30 @@ from django.db import connection, reset_queries
 reset_queries()
 
 
-@api_view(['POST'])
+@api_view(["POST"])
 @permission_classes([IsAuthenticated, IsAdminUser])
 def create_packet_qr_code(request):
     try:
-        num_of_qrcodes = int(request.data['num_of_qrcodes'])
+        num_of_qrcodes = int(request.data["num_of_qrcodes"])
     except (KeyError, TypeError, ValueError):
-        return Response({'error': 'Invalid input'})
-    category = request.data['category']
+        return Response({"error": "Invalid input"})
+    category = request.data["category"]
 
     if num_of_qrcodes <= 0 or num_of_qrcodes > 10000:
-        return Response({'error': 'Number of QR codes must be between 1 and 10,000'})
+        return Response({"error": "Number of QR codes must be between 1 and 10,000"})
 
     qrcodes = create_packet_qr_codes(num_of_qrcodes, category)
 
     if qrcodes:
         serializer = PacketSerializerCreate(qrcodes, many=True)
-        return Response({'success': f'{num_of_qrcodes} QR codes created', 'qr_codes': serializer.data})
+        return Response(
+            {
+                "success": f"{num_of_qrcodes} QR codes created",
+                "qr_codes": serializer.data,
+            }
+        )
     else:
-        return Response({'error': 'QR code creation failed'})
+        return Response({"error": "QR code creation failed"})
 
 
 class EmployeeQrCodeScanerView(APIView):
@@ -64,10 +75,8 @@ class EmployeeQrCodeScanerView(APIView):
                 last_lifecycle.employee = employer
                 last_lifecycle.filled_at = timezone.now()
                 last_lifecycle.save()
-                LifeCycle.objects.create(
-                    box=box
-                )
-                money = box.category.summa * filled/100
+                LifeCycle.objects.create(box=box)
+                money = box.category.summa * filled / 100
 
                 bank_account.capital += money
                 bank_account.save()
@@ -77,16 +86,21 @@ class EmployeeQrCodeScanerView(APIView):
                     amount=money,
                     tarrif=cat,
                     box=box,
-
                 )
-                return Response({'message': "box successfully scaned"}, status=status.HTTP_202_ACCEPTED)
-            
-            return Response({'message': "The box is empty or your level is not suitable"},
-                            status=status.HTTP_403_FORBIDDEN)
-            
+                return Response(
+                    {"message": "box successfully scaned"},
+                    status=status.HTTP_202_ACCEPTED,
+                )
+
+            return Response(
+                {"message": "The box is empty or your level is not suitable"},
+                status=status.HTTP_403_FORBIDDEN,
+            )
+
         # for simple packets
         elif packet_qr_code.first() is not None:
             packet = packet_qr_code.first()
+            user = packet.employee
             cat = packet.category
             if packet.scannered_at is None and cat in employer.categories.all():
                 packet.employee = employer
@@ -98,46 +112,70 @@ class EmployeeQrCodeScanerView(APIView):
                 bank_account.save()
 
                 Earning.objects.create(
-                    bank_account=bank_account,
-                    amount=money,
-                    tarrif=cat,
-                    packet=packet
+                    bank_account=bank_account, amount=money, tarrif=cat, packet=packet
                 )
-                return Response({'message': "packet successfully scaned"}, status=status.HTTP_202_ACCEPTED)
-            return Response({'message': "packet already scaned"}, status=status.HTTP_403_FORBIDDEN)
+                return Response(
+                    {"message": "packet successfully scaned"},
+                    status=status.HTTP_202_ACCEPTED,
+                )
+            elif cat not in employer.categories.all():
+                return Response(
+                    {
+                        "message": "Sizning rolingiz packet uchun to'g'ri kelmaydi! Iltimos adminlarga murojat qiling!"
+                    },
+                    status=status.HTTP_403_FORBIDDEN,
+                )
+            serializer = UserEarningSerializer(user)
+            return Response(
+                {"message": "paket allaqachon skanerlangan!","user":serializer.data}, status=status.HTTP_403_FORBIDDEN
+            )
         # Return a success response
-        return Response({'message': "Packet or Box doesn't exists"}, status=status.HTTP_404_NOT_FOUND)
+        return Response(
+            {"message": "Packet or Box doesn't exists"},
+            status=status.HTTP_404_NOT_FOUND,
+        )
 
 
 class CategoryModelViewSet(viewsets.ModelViewSet):
     serializer_class = CategorySerializer
-    queryset = Category.objects.all().order_by('id')
+    queryset = Category.objects.all().order_by("id")
 
 
 class PacketListAPIView(generics.ListAPIView):
     pagination_class = MyPagination
     serializer_class = PacketSerializer
-    queryset = Packet.objects.all().exclude(
-        scannered_at__isnull=True).only('scannered_at').order_by('-id')
+    queryset = (
+        Packet.objects.all()
+        .exclude(scannered_at__isnull=True)
+        .only("scannered_at")
+        .order_by("-id")
+    )
     filter_backends = [filters.DjangoFilterBackend, rf_filters.SearchFilter]
-    filterset_fields = ['category']
-    search_fields = ['employee__first_name',
-                     'employee__phone_number', 'employee__car_number']
+    filterset_fields = ["category"]
+    search_fields = [
+        "employee__first_name",
+        "employee__phone_number",
+        "employee__car_number",
+    ]
 
 
 class ListOrBulkDeletePacket(APIView, LimitOffsetPagination):
     def get(self, request, *args, **kwargs):
-        date = request.query_params.get('date', None)
-        queryset = Packet.objects.filter(
-            scannered_at__isnull=True).filter(created_at__lte=date).only("category", "created_at")
+        date = request.query_params.get("date", None)
+        queryset = (
+            Packet.objects.filter(scannered_at__isnull=True)
+            .filter(created_at__lte=date)
+            .only("category", "created_at")
+        )
         paginator = MyPagination()
         result_page = paginator.paginate_queryset(queryset, request)
         serializer = PacketGarbageSerializer(result_page, many=True)
         return paginator.get_paginated_response(serializer.data)
 
     def delete(self, request, *args, **kwargs):
-        date = request.query_params.get('date', None)
-        queryset = Packet.objects.filter(
-            scannered_at__isnull=True).filter(created_at__lte=date)
+        date = request.query_params.get("date", None)
+        queryset = Packet.objects.filter(scannered_at__isnull=True).filter(
+            created_at__lte=date
+        )
         queryset.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
