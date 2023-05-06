87ed41257d793f8311c5f0f51841fe92993ecc18
Boburshoh-oss <boburbekbotirov868@gmail.com>
2023-05-06 22:01:06 +0500

    installed app removed giis

--- core/settings.py
+++ core/settings.py
@@ -49,7 +49,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "django.contrib.gis",
+    # "django.contrib.gis",
     "rest_framework",
     "rest_framework.authtoken",
     "corsheaders",
