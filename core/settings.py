8dc68eadc5b014838ed11387b49eb0dfdc5ea4e2
Boburshoh-oss <boburbekbotirov868@gmail.com>
2023-05-08 16:37:43 +0500

    whitenoice

--- core/settings.py
+++ core/settings.py
@@ -68,6 +68,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 # CORS ORIGIN
@@ -165,7 +166,7 @@ STATIC_URL = "static/"
 # /home/tozauzuz/tozauz.uz/django
 
 import os
-STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+STATIC_ROOT = BASE_DIR / 'productionfiles'
 # STATICFILES_DIRS = [BASE_DIR / "static"]
 
 
