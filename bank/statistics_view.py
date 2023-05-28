8767e2f6b2eeefbd9f053da41bd6dd19683b67d1
Boburshoh-oss <boburbekbotirov868@gmail.com>
2023-05-28 22:02:23 +0500

    dashboard info

---
+++ bank/statistics_view.py
@@ -0,0 +1,95 @@
+from django.db.models import Sum
+from django.utils import timezone
+
+from dateutil.relativedelta import relativedelta
+from rest_framework.views import APIView
+from rest_framework.response import Response
+
+from account.models import User
+from ecopacket.models import LifeCycle
+from .models import Earning, PayMe, PayOut
+
+
+def get_month_name(num):
+    return {
+        1: "Yanvar",
+        2: "Fevral",
+        3: "Mart",
+        4: "Aprel",
+        5: "May",
+        6: "Iyun",
+        7: "Iyul",
+        8: "Avgust",
+        9: "Sentabr",
+        10: "Oktabr",
+        11: "Noyabr",
+        12: "Dekabr",
+    }[num]
+
+
+def sum_amount(model, datetime):
+    return (
+        model.objects.filter(
+            created_at__year=datetime.year, created_at__month=datetime.month
+        ).aggregate(total=Sum("amount"))["total"]
+        or 0
+    )
+
+
+def get_monthly_sums():
+    current_datetime = timezone.now()
+
+    monthly_sums = []
+
+    for sup_month in range(12):
+        dt = current_datetime - relativedelta(months=sup_month)
+
+        monthly_earning = sum_amount(Earning, dt)
+
+        monthly_payout = sum_amount(PayOut, dt)
+
+        monthly_payme = sum_amount(PayMe, dt)
+
+        monthly_sums.append(
+            {
+                "name": get_month_name(dt.month),
+                "earning": monthly_earning,
+                "payout": monthly_payout,
+                "payme": monthly_payme,
+            }
+        )
+
+    return monthly_sums[::-1]
+
+
+def get_header_info():
+    pops_count = User.objects.filter(role="POP").count()
+    emps_count = User.objects.filter(role="EMP").count()
+    earings_amount = Earning.objects.aggregate(sum=Sum("amount"))["sum"]
+    orders_count = LifeCycle.objects.filter(filled_at=None, state__gt=80).count()
+
+    return {
+        "pops": pops_count,
+        "emps": emps_count,
+        "earnings": earings_amount,
+        "orders": orders_count,
+    }
+
+
+class DashboardView(APIView):
+    def get(self):
+        chart_data = get_monthly_sums()
+        featured_data = chart_data[-1]
+        return Response(
+            {
+                "header_cards": get_header_info(),
+                "chart_data": chart_data,
+                "featured": {
+                    "percentage": round(
+                        featured_data["payout"] / featured_data["payme"] * 100
+                    ),
+                    "needed_to_pay": featured_data["payme"] - featured_data["payout"],
+                    "target": featured_data,
+                },
+            }
+        )
--- core/urls.py
+++ core/urls.py
@@ -2,18 +2,17 @@ from django.conf import settings
 from django.conf.urls.static import static
 from .yasg import swaggerurlpatterns
 from django.contrib import admin
-from django.urls import path, include, re_path
-from django.views.generic import TemplateView
+from django.urls import path, include
+from bank.statistics_view import DashboardView
 
 urlpatterns = [
-    path('__debug__/', include('debug_toolbar.urls')),
-    path('admin/', admin.site.urls),
-
-    path('api/v1/account/', include('account.urls')),
-    path('api/v1/ecopacket/', include('ecopacket.urls')),
-    path('api/v1/packet/', include('packet.urls')),
-    path('api/v1/bank/', include('bank.urls')),
-    
+    path("__debug__/", include("debug_toolbar.urls")),
+    path("admin/", admin.site.urls),
+    path("api/v1/account/", include("account.urls")),
+    path("api/v1/ecopacket/", include("ecopacket.urls")),
+    path("api/v1/packet/", include("packet.urls")),
+    path("api/v1/bank/", include("bank.urls")),
+    path("api/v1/dashboard/", DashboardView.as_view()),
 ]
 
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
