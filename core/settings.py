cc1e1b1722b4b0d69e36f005d20dfba57f8963d2
Boburshoh-oss <boburbekbotirov868@gmail.com>
2023-06-16 23:03:26 +0500

    dastabase

--- core/settings.py
+++ core/settings.py
@@ -102,27 +102,27 @@ AUTH_USER_MODEL = "account.User"
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-DATABASES = {
-    "default": {
-        "ENGINE": "django.contrib.gis.db.backends.postgis",
-        "NAME": "geodjango",
-        "USER": "geodjango",
-        "PASSWORD": "12345",
-        "HOST": "localhost",
-        "PORT": "5432",
-    }
-}
-
 # DATABASES = {
 #     "default": {
-#         "ENGINE": "django.db.backends.postgresql_psycopg2",
-#         "NAME": "tozauz",
-#         "USER": "tozauz",
-#         "PASSWORD": "tozauz",
+#         "ENGINE": "django.contrib.gis.db.backends.postgis",
+#         "NAME": "geodjango",
+#         "USER": "geodjango",
+#         "PASSWORD": "12345",
 #         "HOST": "localhost",
-#         "PORT": "",
+#         "PORT": "5432",
 #     }
 # }
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.postgresql_psycopg2",
+        "NAME": "tozauz",
+        "USER": "tozauz",
+        "PASSWORD": "tozauz",
+        "HOST": "localhost",
+        "PORT": "",
+    }
+}
 
 
 
