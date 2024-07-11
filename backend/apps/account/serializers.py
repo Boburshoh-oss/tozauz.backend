df3d8d16e0a42cb0d5c40be81214e8e957c5a057
NajmiddinG <najmiddinweb@gmail.com>
2024-07-11 22:47:31 +0500

    added-user-update-api

--- backend/apps/account/serializers.py
+++ backend/apps/account/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from .models import User
+from .models import User, RoleOptions
 from django.contrib.auth.hashers import make_password
 from django.utils.translation import gettext_lazy as _
 
@@ -139,3 +139,18 @@ class OTPSerializer(serializers.Serializer):
     phone_number = serializers.CharField()
     otp = serializers.CharField()
     new_password = serializers.CharField(write_only=True)
+
+
+class UserProfileUpdateSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ('first_name', 'last_name', 'car_number')
+        extra_kwargs = {
+            'car_number': {'required': False}
+        }
+
+    def validate(self, data):
+        user = self.instance
+        if user.role not in [RoleOptions.EMPLOYE, RoleOptions.ADMIN] and 'car_number' in data:
+            raise serializers.ValidationError("Siz moshina raqamini o'zgartira olmaysiz!")
+        return data
