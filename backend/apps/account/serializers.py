c8d4b057049fb0e25ea3eb05888b9da1045a0c32
Botirov Boburbek <74315658+Boburshoh-oss@users.noreply.github.com>
2024-07-30 14:01:14 +0500

    password set

--- backend/apps/account/serializers.py
+++ backend/apps/account/serializers.py
@@ -38,16 +38,20 @@ class UserRegisterSerializer(serializers.ModelSerializer):
         }
 
     def create(self, validated_data):
-        # if "password" in validated_data:
-        #     validated_data["password"] = make_password(
-        #         validated_data["password"])
-        return super().create(validated_data)
-
+        password = validated_data.pop('password', None)
+        user = super().create(validated_data)
+        if password:
+            user.set_password(password)
+            user.save()
+        return user
+    
     def update(self, instance, validated_data):
-        # if "password" in validated_data:
-        #     validated_data["password"] = make_password(
-        #         validated_data["password"])
-        return super().update(instance, validated_data)
+        password = validated_data.pop('password', None)
+        user = super().update(instance, validated_data)
+        if password:
+            user.set_password(password)
+            user.save()
+        return user
 
 
 class UserAdminRetrieveSerializer(serializers.ModelSerializer):
