bcb2920da63d95cc625ccd36dc16007f9a0d0756
Boburshoh-oss <Boburbekbotirov868@gmail.com>
2024-08-09 16:12:33 +0500

    filter some features

--- backend/apps/bank/admin.py
+++ backend/apps/bank/admin.py
@@ -1,7 +1,14 @@
 from django.contrib import admin
 from .models import BankAccount, Earning, PayOut, PayMe
 # Register your models here.
-admin.site.register(BankAccount)
-admin.site.register(Earning)
+@admin.register(BankAccount)
+class BankAccountAdmin(admin.ModelAdmin):
+    search_fields = ["user__phone_number","user__first_name","user__last_name"]
+    ordering = ["id"]
+@admin.register(Earning)
+class EarningAdmin(admin.ModelAdmin):
+    list_display = ["amount", "penalty_amount"]
+    list_filter = ["tarrif","box","is_penalty"]
+    raw_id_fields = ["packet"]
 admin.site.register(PayOut)
 admin.site.register(PayMe)
