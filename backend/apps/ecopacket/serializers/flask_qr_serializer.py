da73747c771fa6e00d6a48ecf6f20e7687b51d28
Boburshoh-oss <boburbekbotirov868@gmail.com>
2025-11-19 14:37:28 +0500

    enhance FlaskQrCodeSerializer to include category name; update FlaskQrCodeViewSet to optimize queryset with select_related and annotate

--- backend/apps/ecopacket/serializers/flask_qr_serializer.py
+++ backend/apps/ecopacket/serializers/flask_qr_serializer.py
@@ -1,8 +1,11 @@
 from rest_framework import serializers
 from apps.ecopacket.models import FlaskQrCode
 
+
 class FlaskQrCodeSerializer(serializers.ModelSerializer):
+    category__name = serializers.CharField(read_only=True)
+
     class Meta:
         model = FlaskQrCode
-        fields = ['id', 'bar_code', 'category', 'created_at']
-        read_only_fields = ['created_at']
+        fields = ["id", "bar_code", "category", "created_at", "category__name"]
+        read_only_fields = ["created_at"]
--- backend/apps/ecopacket/views/flask_qr_viewset.py
+++ backend/apps/ecopacket/views/flask_qr_viewset.py
@@ -1,16 +1,28 @@
 from rest_framework import viewsets, filters
 from rest_framework.permissions import IsAuthenticated
 from django_filters.rest_framework import DjangoFilterBackend
-
+from django.db.models import F
 from apps.ecopacket.models import FlaskQrCode
 from apps.ecopacket.serializers.flask_qr_serializer import FlaskQrCodeSerializer
+
 
 class FlaskQrCodeViewSet(viewsets.ModelViewSet):
     queryset = FlaskQrCode.objects.all()
     serializer_class = FlaskQrCodeSerializer
     permission_classes = [IsAuthenticated]
-    filter_backends = [DjangoFilterBackend, filters.SearchFilter, filters.OrderingFilter]
-    filterset_fields = ['category']
-    search_fields = ['bar_code']
-    ordering_fields = ['created_at']
-    ordering = ['-created_at']  # Default ordering
+    filter_backends = [
+        DjangoFilterBackend,
+        filters.SearchFilter,
+        filters.OrderingFilter,
+    ]
+    filterset_fields = ["category"]
+    search_fields = ["bar_code"]
+    ordering_fields = ["created_at"]
+    ordering = ["-created_at"]  # Default ordering
+
+    def get_queryset(self):
+        return (
+            FlaskQrCode.objects.select_related("category")
+            .annotate(category_name=F("category__name"))
+            .all()
+        )
