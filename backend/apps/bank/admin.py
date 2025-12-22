e37b78c5e6d5f886578ef6164cbecf4302bf0fc4
Boburshoh-oss <boburbekbotirov868@gmail.com>
2025-12-22 16:26:39 +0500

    feat: add QrCheckLogAdmin for managing QR check logs in admin panel

--- backend/apps/bank/admin.py
+++ backend/apps/bank/admin.py
@@ -1,14 +1,26 @@
 from django.contrib import admin
-from .models import BankAccount, Earning, PayOut, PayMe
+from .models import BankAccount, Earning, PayOut, PayMe, QrCheckLog
+
+
 # Register your models here.
 @admin.register(BankAccount)
 class BankAccountAdmin(admin.ModelAdmin):
-    search_fields = ["user__phone_number","user__first_name","user__last_name"]
+    search_fields = ["user__phone_number", "user__first_name", "user__last_name"]
     ordering = ["id"]
+
+
 @admin.register(Earning)
 class EarningAdmin(admin.ModelAdmin):
     list_display = ["amount", "penalty_amount"]
-    list_filter = ["tarrif","box","is_penalty"]
+    list_filter = ["tarrif", "box", "is_penalty"]
     raw_id_fields = ["packet"]
+
+
+class QrCheckLogAdmin(admin.ModelAdmin):
+    list_display = ["qr_code", "request_time", "ip_address", "qr_type", "exists"]
+    list_filter = ["qr_type", "exists", "request_time"]
+    search_fields = ["qr_code", "ip_address", "user_agent"]
+
+
 admin.site.register(PayOut)
 admin.site.register(PayMe)
